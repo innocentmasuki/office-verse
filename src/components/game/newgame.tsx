de4f1c4350ec31e991bd11adca22f32531c0bdd6
innocentmasuki <innocentmasuki99@gmail.com>
2023-11-23 19:43:15 +0200

    changed, the ipAddress

--- src/components/game/newgame.tsx
+++ src/components/game/newgame.tsx
@@ -14,7 +14,7 @@ type GameProps = {
 
 
 
-export const gameSocket =process.env.NODE_ENV === "development" ? io( "http://localhost:3001") : io("http://officeverse.com", { path: '/socket.io/' });
+export const gameSocket =process.env.NODE_ENV === "development" ? io( "http://localhost:3001") : io("http://10.0.254.254", { path: '/socket.io/' });
 
 export const Game: React.FC<GameProps> = ({ walls}) => {
 

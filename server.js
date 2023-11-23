819e090ba0d986b9e30018435329352129680bf6
innocentmasuki <innocentmasuki99@gmail.com>
2023-11-23 17:26:56 +0200

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://officeverse.com",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
